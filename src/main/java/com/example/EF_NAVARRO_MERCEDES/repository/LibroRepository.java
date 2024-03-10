0c65628e6ee1bcb08b2a0cb7f4264d3395db068e
MercedesIsabel22 <142359700+MercedesIsabel22@users.noreply.github.com>
2024-03-10 10:48:33 -0500

    Update LibroRepository.java

--- src/main/java/com/example/EF_NAVARRO_MERCEDES/repository/LibroRepository.java
+++ src/main/java/com/example/EF_NAVARRO_MERCEDES/repository/LibroRepository.java
@@ -8,6 +8,5 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface LibroRepository extends JpaRepository<Libro, Long> {
-    @Query(value = "SELECT * FROM libros l WHERE l.idgenero = :idGenero AND l.nombre LIKE LOWER(CONCAT('%', :nombre, '%'))", nativeQuery = true)
-    List<Libro> findByNombreAndGenero(@Param("nombre") String nombre, @Param("idGenero") Long idGenero);
+    
 }
